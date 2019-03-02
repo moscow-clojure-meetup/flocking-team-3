83a84eab0ce0fcac2bacb4427f96ecfdd80f4f4c
Aleksey Pirogov <astynax@users.noreply.github.com>
2019-03-02 17:44:24 +0300

    add a monet for drawing

--- src/main.cljs
+++ src/main.cljs
@@ -1,4 +1,18 @@
-(ns main)
+(ns main
+  (:require [monet.canvas :as canvas]))
+
+(def canvas-dom (.getElementById js/document "canvas"))
+
+(def monet-canvas (canvas/init canvas-dom "2d"))
+
+
 
 (defn main! []
-  (println "foo"))
+  (canvas/add-entity
+   monet-canvas :background
+   (canvas/entity {:x 0 :y 0 :w 600 :h 600} ; val
+                  nil                       ; update function
+                  (fn [ctx val]             ; draw function
+                    (-> ctx
+                        (canvas/fill-style "#191d21")
+                        (canvas/fill-rect val))))))
